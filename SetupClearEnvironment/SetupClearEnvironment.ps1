46f660010cd8aeb19d3156cf9f1e9a470cfa07d1
pavelski01 <pavelski01@gmail.com>
2026-03-08 23:49:40 +0100

    Check if stack exists otherwise create

--- SetupClearEnvironment/SetupClearEnvironment.ps1
+++ SetupClearEnvironment/SetupClearEnvironment.ps1
@@ -1,4 +1,9 @@
-docker compose -f .\compose.yaml up -d
+$stackName = "azurite-stack"
+$existing = docker compose -p $stackName ps --quiet 2>$null
+if (-not $existing) {
+	Write-Host "Starting stack '$stackName'" -ForegroundColor Green
+    docker compose -f .\compose.yaml up -d
+}
 .\Delete-Database.ps1 -ServerInstance "(localdb)\mssqllocaldb" -DatabaseName aspnet-53bc9b9d-9d6a-45d4-8429-2a2761773502 -Force
 .\Delete-AzureStorage.ps1 -ConnectionString "UseDevelopmentStorage=true" -TableName OrleansSiloInstances -Force
 .\Delete-AzureStorage.ps1 -ConnectionString "UseDevelopmentStorage=true" -TableName OrleansReminders -Force
